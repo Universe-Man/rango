fd6d6507c19dac7929bc0700c876d67f4f2c2c7d
Ian Pollack <ianjpollack@gmail.com>
2021-03-22 18:36:21 -0700

    added form for page but missing 1 arg

--- tango_with_django_project/rango/forms.py
+++ tango_with_django_project/rango/forms.py
@@ -18,7 +18,15 @@ class PageForm(forms.ModelForm):
         max_length=128, help_text="Please enter the title of the page."
     )
     url = forms.URLField(max_length=200, help_text="Please enter the URL of the page.")
-    views = forms.IntegerField(widget=forms.HiddenField(), initial=0)
+    views = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
+
+    def clean():
+        cleaned_data = self.cleaned_data
+        url = cleaned_data.get("url")
+        if url and not url.startswith("http://"):
+            url = "http://" + url
+            cleaned_data["url"] = url
+            return cleaned_data
 
     class Meta:
         model = Page
